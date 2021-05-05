767eb5ad610778327cb2da01e1e4aac0db4fe2b7
DANIEL SILVA PEREIRA <daniel.spereira22@senacsp.edu.br>
2021-05-05 17:19:16 -0300

    novo app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,10 @@ const routes: Routes = [
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'add-usuario',
+    loadChildren: () => import('./add-usuario/add-usuario.module').then( m => m.AddUsuarioPageModule)
   }
 ];
 
